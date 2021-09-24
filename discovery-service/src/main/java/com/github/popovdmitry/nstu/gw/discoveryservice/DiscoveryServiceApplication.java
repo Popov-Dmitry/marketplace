326339fd7fec1b80d18a750c0895dd51faf6b5bb
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-09-24 19:49:02 +0700

    Init discovery-service

---
+++ discovery-service/src/main/java/com/github/popovdmitry/nstu/gw/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
