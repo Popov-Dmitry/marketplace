f621db1df29d720d75bc076cc828e2461df3f04b
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-09-25 14:34:03 +0700

    Added Customer model

---
+++ customer-service/src/main/java/com/github/popovdmitry/nstu/gw/customerservice/model/Customer.java
@@ -0,0 +1,33 @@
+package com.github.popovdmitry.nstu.gw.customerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
