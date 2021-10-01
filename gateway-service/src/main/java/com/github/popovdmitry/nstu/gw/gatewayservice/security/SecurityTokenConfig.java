f8993e8a8263491eabfac05584b7ca8abcb4d321
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-10-01 23:48:19 +0700

    Added SecurityTokenConfig

---
+++ gateway-service/src/main/java/com/github/popovdmitry/nstu/gw/gatewayservice/security/SecurityTokenConfig.java
@@ -0,0 +1,33 @@
+package com.github.popovdmitry.nstu.gw.gatewayservice.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.jwt.uri}")
+    private String authUri;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint((httpServletRequest, httpServletResponse, e) ->
+                        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .addFilterAfter(new JwtTokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, authUri).permitAll()
+                .anyRequest().authenticated();
+    }
+}
