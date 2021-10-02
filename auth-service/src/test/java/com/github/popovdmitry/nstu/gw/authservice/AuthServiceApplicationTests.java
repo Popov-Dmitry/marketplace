3d369c486e4923abaa7c084ae1cfa11e076af180
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-10-02 14:51:27 +0700

    Init auth service

---
+++ auth-service/src/test/java/com/github/popovdmitry/nstu/gw/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
