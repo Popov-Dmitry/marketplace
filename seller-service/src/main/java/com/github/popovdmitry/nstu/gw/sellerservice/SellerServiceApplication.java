1eeab9721b5e88123bff3800029516668885b54b
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-10-15 15:31:58 +0700

    Init seller-service

---
+++ seller-service/src/main/java/com/github/popovdmitry/nstu/gw/sellerservice/SellerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.sellerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerServiceApplication.class, args);
+    }
+
+}
---
+++ seller-service/src/test/java/com/github/popovdmitry/nstu/gw/sellerservice/SellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.sellerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
