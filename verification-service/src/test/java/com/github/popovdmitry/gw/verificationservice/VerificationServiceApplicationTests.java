c4e914e7deb98004c0a6d88ea32ae0c9eac0a832
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-10-21 23:24:52 +0700

    Init verification-service

---
+++ verification-service/src/test/java/com/github/popovdmitry/gw/verificationservice/VerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.gw.verificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
