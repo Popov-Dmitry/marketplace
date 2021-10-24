6c07139b46c2d4b554516ac46e260695cd05bc86
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-10-24 13:54:55 +0700

    Added configs for verification-service

--- verification-service/src/main/java/com/github/popovdmitry/gw/verificationservice/VerificationServiceApplication.java
+++ verification-service/src/main/java/com/github/popovdmitry/gw/verificationservice/VerificationServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.popovdmitry.gw.verificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VerificationServiceApplication {
 
     public static void main(String[] args) {
