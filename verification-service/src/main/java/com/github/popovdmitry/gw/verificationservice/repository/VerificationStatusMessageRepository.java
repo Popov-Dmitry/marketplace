0d21291293ce5500ba014bb1652afd06bd9e46d0
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-10-30 20:00:24 +0700

    Added VerificationStatusMessageService

--- verification-service/src/main/java/com/github/popovdmitry/gw/verificationservice/repository/VerificationStatusMessageRepository.java
+++ verification-service/src/main/java/com/github/popovdmitry/gw/verificationservice/repository/VerificationStatusMessageRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface VerificationStatusMessageRepository extends JpaRepository<VerificationStatusMessage, Long> {
     Optional<VerificationStatusMessage> findBySellerId(Long sellerId);
+    void deleteBySellerId(Long sellerId);
 }
