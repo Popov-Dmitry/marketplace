6b1d08994d81ae3535ae791e573c31d79aa746ec
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-11-02 21:43:30 +0700

    Init configuration-service

---
+++ configuration-service/src/main/java/com/github/popovdmitry/gw/configurationservice/ConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.gw.configurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+
+}
