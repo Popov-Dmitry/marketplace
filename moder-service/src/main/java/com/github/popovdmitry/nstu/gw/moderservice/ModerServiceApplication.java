21b8d754e3598beb197dc26959175caf241289e5
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-11-07 16:17:56 +0700

    Init moder-service

---
+++ moder-service/src/main/java/com/github/popovdmitry/nstu/gw/moderservice/ModerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.moderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModerServiceApplication.class, args);
+    }
+
+}
---
+++ moder-service/src/test/java/com/github/popovdmitry/nstu/gw/moderservice/ModerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.moderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
