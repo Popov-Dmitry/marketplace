314f89ee9dfd952bff3794d47d6fd08df62777f9
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-11-07 15:45:30 +0700

    Added configs for configuration-service

--- configuration-service/src/main/java/com/github/popovdmitry/gw/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/github/popovdmitry/gw/configurationservice/ConfigurationServiceApplication.java
@@ -2,8 +2,12 @@ package com.github.popovdmitry.gw.configurationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
