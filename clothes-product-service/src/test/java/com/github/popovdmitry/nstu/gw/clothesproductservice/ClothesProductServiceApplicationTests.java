c406d7a02628810874e2999fdbae04474f4519f4
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-11-10 20:03:57 +0700

    Init clothes-product-service

---
+++ clothes-product-service/src/test/java/com/github/popovdmitry/nstu/gw/clothesproductservice/ClothesProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.clothesproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
