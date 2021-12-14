7965b9cdab6f5ac2e95379bf79d23c9e8d27c165
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-14 15:49:33 +0700

    Added ClothesProductDto

---
+++ clothes-product-service/src/main/java/com/github/popovdmitry/nstu/gw/clothesproductservice/dto/ClothesProductDto.java
@@ -0,0 +1,25 @@
+package com.github.popovdmitry.nstu.gw.clothesproductservice.dto;
+
+import com.github.popovdmitry.nstu.gw.clothesproductservice.model.Category;
+import com.github.popovdmitry.nstu.gw.clothesproductservice.model.Season;
+import com.github.popovdmitry.nstu.gw.clothesproductservice.model.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClothesProductDto {
+
+    private String color;
+    private Size size;
+
+    private String brand;
+    private String title;
+    private Category category;
+    private Season season;
+    private String type;
+}
