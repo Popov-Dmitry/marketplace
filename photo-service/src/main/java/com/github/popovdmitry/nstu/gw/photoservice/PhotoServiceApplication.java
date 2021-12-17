42218b88a4beb4b3c4e414bd66c74a299b8b0d7a
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-17 20:34:24 +0700

    Init photo service

---
+++ photo-service/src/main/java/com/github/popovdmitry/nstu/gw/photoservice/PhotoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.photoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoServiceApplication.class, args);
+    }
+
+}
---
+++ photo-service/src/test/java/com/github/popovdmitry/nstu/gw/photoservice/PhotoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.photoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
