57a3186411641631d0d1682903ff1363b4cd3940
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-03-27 18:54:26 +0700

    Added initial site routes

---
+++ client/src/utils/consts.js
@@ -0,0 +1,18 @@
+export const MAIN_ROUTE = "/";
+export const REGISTRATION_ROUTE = "/registration";
+export const ACCOUNT_ROUTE = "/account";
+export const SEARCH_ROUTE = "/search";
+export const CATALOG_ROUTE = "/catalog";
+export const CLOTHES_ROUTE = "/clothes";
+
+export const SELLER_ROUTE = "/seller";
+export const MODER_ROUTE = "/moder";
+export const ADMIN_ROUTE = "/admin";
+
+export const CART_ROUTE = "/my/cart";
+export const ORDERS_ROUTE = "/my/orders";
+export const WISHLIST_ROUTE = "/my/wishlist";
+
+export const SELLER_SHOP_ROUTE = "/my/shop";
+export const SELLER_PRODUCTS_ROUTE = "/my/products";
+export const SELLER_NEW_PRODUCT_ROUTE = "/my/products/new";
