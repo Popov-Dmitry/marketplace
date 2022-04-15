f7bcf299983bd28a4c2e7d5a5541584db74ae465
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-04-15 15:08:19 +0700

    Added PersonalData component

--- client/src/components/DatePicker.js
+++ client/src/components/DatePicker.js
@@ -1,21 +1,22 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {FormSelect} from "react-bootstrap";
 
 const months = ['Янв', 'Фев', 'Мар', 'Апр',
     'Мая', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт',
     'Ноя', 'Дек'];
 
-let years = [];
 
-const calculateYears = () => {
-    let year = new Date().getFullYear();
+const calculateYears = (maxYear) => {
+    let years = [];
+    let year = maxYear ? maxYear : new Date().getFullYear();
     for(let i = 0; i < 101; i++) {
         years.push(year - i);
     }
+    return years;
 }
 
+let dayNum;
 const calculateDays = (year, month) => {
-    let dayNum;
     if (month === 'Янв' || month === 'Мар' ||
         month === 'Мая' || month === 'Июл' || month === 'Авг'
         || month === 'Окт' || month === 'Дек') {
@@ -40,31 +41,40 @@ const calculateDays = (year, month) => {
     return days;
 }
 
-calculateYears();
 
-
-const DatePicker = () => {
-    const [day, setDay] = useState(1);
-    const [month, setMonth] = useState('Янв');
-    const [year, setYear] = useState(2022);
+const DatePicker = ({maxYear, initialDate, onChange}) => {
+    const [day, setDay] = useState(initialDate.day ? initialDate.day : 1);
+    const [month, setMonth] = useState(initialDate.month ? initialDate.month : 'Янв');
+    const [year, setYear] = useState(initialDate.year ? initialDate.year : maxYear ? maxYear : new Date().getFullYear());
+    const years = useMemo(() => calculateYears(maxYear), [maxYear]);
     const days = useMemo(() => calculateDays(year, month), [year, month]);
+
+    useEffect(() => {
+        if (day > dayNum) {
+            setDay(1);
+        }
+        onChange({day, month, year})
+    }, [day, month, year]);
 
     return (
         <div className="d-flex">
             <FormSelect
                 value={day}
+                className={"border-radius-10 me-1"}
                 onChange={e => setDay(e.target.value)}
             >
                 {days.map(d => <option key={d} value={d}>{d}</option>)}
             </FormSelect>
             <FormSelect
                 value={month}
+                className={"border-radius-10 me-1"}
                 onChange={e => setMonth(e.target.value)}
             >
                 {months.map(m => <option key={m} value={m}>{m}</option>)}
             </FormSelect>
             <FormSelect
                 value={year}
+                className={"border-radius-10"}
                 onChange={e => setYear(e.target.value)}
             >
                 {years.map(y => <option key={y} value={y}>{y}</option>)}
---
+++ client/src/components/account/PersonalData.js
@@ -0,0 +1,95 @@
+import React, {useState} from 'react';
+import {Button, Col, Form, Row} from "react-bootstrap";
+import {useSelector} from "react-redux";
+import DatePicker from "../DatePicker";
+
+const PersonalData = () => {
+    const account = useSelector(state => state.userReducer);
+    const [firstName, setFirstName] = useState(account.user.firstName);
+    const [secondName, setSecondName] = useState(account.user.secondName);
+    const [sex, setSex] = useState(account.user.sex);
+    const [birthDay, setBirthDay] = useState(account.user.birthDay);
+    const [birthMonth, setBirthMonth] = useState(account.user.birthMonth);
+    const [birthYear, setBirthYear] = useState(account.user.birthYear);
+
+
+    const changeChecker = () => {
+        if (document.getElementById("save")) {
+            if (account.user.firstName !== firstName || account.user.secondName !== secondName ||
+                account.user.sex !== sex || account.user.birthDay !== birthDay ||
+                account.user.birthMonth !== birthMonth || account.user.birthYear !== birthYear) {
+                document.getElementById("save").classList.remove("disabled");
+            }
+            else {
+                document.getElementById("save").classList.add("disabled");
+            }
+        }
+    }
+
+    changeChecker();
+
+    return (
+        <div>
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Label className={"opacity-95"}>Имя</Form.Label>
+                        <Form.Control
+                            className={"mb-2 border-radius-10"}
+                            placeholder="Имя"
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                        />
+                    </Col>
+                    <Col>
+                        <Form.Label className={"opacity-95"}>Фамилия</Form.Label>
+                        <Form.Control
+                            className={"mb-2 border-radius-10"}
+                            placeholder="Фамилия"
+                            value={secondName}
+                            onChange={e => setSecondName(e.target.value)}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Label className={"opacity-95"}>Пол</Form.Label>
+                        <Form.Select
+                            className={"mb-2 border-radius-10"}
+                            value={sex}
+                            onChange={e => setSex(e.target.value)}
+                        >
+                            <option value="MALE">Мужской</option>
+                            <option value="FEMALE">Женский</option>
+                        </Form.Select>
+                    </Col>
+                    <Col>
+                        <Form.Label className={"opacity-95"}>Дата рождения</Form.Label>
+                        <DatePicker
+                            maxYear={new Date().getFullYear() - 7}
+                            initialDate={{
+                                day: birthDay,
+                                month: birthMonth,
+                                year: birthYear
+                        }}
+                            onChange={date => {
+                                setBirthDay(date.day);
+                                setBirthMonth(date.month);
+                                setBirthYear(date.year);
+                            }}
+                        />
+                    </Col>
+                </Row>
+                <Button
+                    id={"save"}
+                    variant={"main"}
+                    className={"mt-2 float-end disabled"}
+                >
+                    Сохранить
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default PersonalData;
