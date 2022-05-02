5f079cfae3874f986b7786218ca61188c14b2659
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-02 17:18:48 +0700

    Added sellerApi

---
+++ client/src/http/sellerApi.js
@@ -0,0 +1,19 @@
+import {$authHost, $host} from "./index";
+import {ADD_SELLER, GET_SELLER, UPDATE_SELLER} from "../utils/endpoints";
+
+export const registrationSeller = async (firstName, secondName, email, password, shopName, country, organizationType, inn, legalAddress) => {
+    const {data} = await $host.post(ADD_SELLER,
+        {firstName, secondName, email, password, shopName, country, organizationType, inn, legalAddress});
+    return data;
+}
+
+export const fetchSellerById = async (id) => {
+    const {data} = await $authHost.get(GET_SELLER + id)
+    return data;
+}
+
+export const updateSeller = async (id, firstName, secondName, email, password, shopName, country, organizationType, inn, legalAddress) => {
+    const {data} = await $authHost.patch(UPDATE_SELLER + id,
+        {firstName, secondName, email, password, shopName, country, organizationType, inn, legalAddress});
+    return data;
+}
