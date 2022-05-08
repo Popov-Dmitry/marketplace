8da2f532bb3530aa7edc6d1be3389a17c57a7b24
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-08 17:27:39 +0700

    Added verificationApi

---
+++ client/src/http/verificationApi.js
@@ -0,0 +1,17 @@
+import {$authHost} from "./index";
+import {GET_ALL_SELLERS_INFO, GET_SELLER_INFO, UPDATE_SELLER_INFO} from "../utils/endpoints";
+
+export const fetchAllSellersInfo = async () => {
+    const {data} = await $authHost.get(GET_ALL_SELLERS_INFO)
+    return data;
+}
+
+export const fetchSellerInfoById = async (id) => {
+    const {data} = await $authHost.get(GET_SELLER_INFO + id)
+    return data;
+}
+
+export const updateSellerInfoById = async (id, verificationStatus, message) => {
+    const {data} = await $authHost.post(UPDATE_SELLER_INFO + id, { verificationStatus, message });
+    return data;
+}
