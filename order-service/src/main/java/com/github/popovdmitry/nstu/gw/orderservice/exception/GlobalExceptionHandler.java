9749232271dd49b12156693861c4ec30b0847f0b
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-09 18:49:06 +0700

    Added GlobalExceptionHandler and ApiErrorDto

---
+++ order-service/src/main/java/com/github/popovdmitry/nstu/gw/orderservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.github.popovdmitry.nstu.gw.orderservice.exception;
+
+import com.github.popovdmitry.nstu.gw.orderservice.dto.ApiErrorDto;
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    ResponseEntity<ApiErrorDto> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ApiErrorDto(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorDto> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiErrorDto(e.getMessage()));
+    }
+}
