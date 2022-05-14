6b2997c25d9cee15aaa7a2ff9ca373734df38fe5
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-14 17:43:47 +0700

    Init delivery-service

---
+++ delivery-service/src/main/java/com/github/popovdmitry/nstu/gw/deliveryservice/DeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ delivery-service/src/test/java/com/github/popovdmitry/nstu/gw/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
