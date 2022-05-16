0af9af24aca206f131214c371e8e4755ca465e4a
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-16 19:59:59 +0700

    Added WishRepository

---
+++ wishlist-service/src/main/java/com/github/popovdmitry/nstu/gw/wishlistservice/repository/WishRepository.java
@@ -0,0 +1,15 @@
+package com.github.popovdmitry.nstu.gw.wishlistservice.repository;
+
+import com.github.popovdmitry.nstu.gw.wishlistservice.model.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    List<Wish> findAllByCustomerId(Long customerId);
+    List<Wish> findAllBySellerId(Long sellerId);
+    List<Wish> findAllByProductDetailsId(Long productDetailsId);
+    List<Wish> findAllByProductId(Long productId);
+}
