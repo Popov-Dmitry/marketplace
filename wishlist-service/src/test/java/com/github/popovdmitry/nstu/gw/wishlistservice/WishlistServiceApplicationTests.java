10559a13d1e27cac61a159f56b3d4388852f406f
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-16 19:44:15 +0700

    Init wishlist-service

---
+++ wishlist-service/src/test/java/com/github/popovdmitry/nstu/gw/wishlistservice/WishlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.nstu.gw.wishlistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
