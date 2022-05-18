9213d9a7724ab04592e585c31062855a74358d42
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2022-05-18 14:39:02 +0700

    Added swagger documentation to wishlist-service

--- wishlist-service/src/main/java/com/github/popovdmitry/nstu/gw/wishlistservice/dto/WishDto.java
+++ wishlist-service/src/main/java/com/github/popovdmitry/nstu/gw/wishlistservice/dto/WishDto.java
@@ -1,6 +1,7 @@
 package com.github.popovdmitry.nstu.gw.wishlistservice.dto;
 
 import com.github.popovdmitry.nstu.gw.wishlistservice.model.ProductType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -10,9 +11,18 @@ import lombok.*;
 @ToString
 public class WishDto {
 
+    @ApiModelProperty(example = "CLOTHES")
     private ProductType productType;
+
+    @ApiModelProperty(example = "123")
     private Long productDetailsId;
+
+    @ApiModelProperty(example = "12345")
     private Long productId;
+
+    @ApiModelProperty(example = "111")
     private Long customerId;
+
+    @ApiModelProperty(example = "321")
     private Long sellerId;
 }
--- wishlist-service/src/main/java/com/github/popovdmitry/nstu/gw/wishlistservice/model/Wish.java
+++ wishlist-service/src/main/java/com/github/popovdmitry/nstu/gw/wishlistservice/model/Wish.java
@@ -1,5 +1,6 @@
 package com.github.popovdmitry.nstu.gw.wishlistservice.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -15,20 +16,26 @@ public class Wish {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(example = "341")
     private Long id;
 
     @Column(name = "product_type")
+    @ApiModelProperty(example = "CLOTHES")
     private ProductType productType;
 
     @Column(name = "product_details_id")
+    @ApiModelProperty(example = "123")
     private Long productDetailsId;
 
     @Column(name = "product_id")
+    @ApiModelProperty(example = "12345")
     private Long productId;
 
     @Column(name = "customer_id")
+    @ApiModelProperty(example = "111")
     private Long customerId;
 
     @Column(name = "seller_id")
+    @ApiModelProperty(example = "321")
     private Long sellerId;
 }
